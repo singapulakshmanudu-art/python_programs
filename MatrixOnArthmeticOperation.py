3973f31ed44770e454a0c0665c34c7df88b59296
Singapu Lakshmanudu <singapulakshmanudu@gmail.com>
2025-12-22 13:23:12 +0530

    Create MatrixOnArthmeticOperation.py

---
+++ MatrixOnArthmeticOperation.py
@@ -0,0 +1,10 @@
+import  numpy as np
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+b=np.array([[5,7,9],[4,3,9],[8,4,7]])
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition:",add)
+print("substraction:",sub)
+print("Multiplication:",mul)
